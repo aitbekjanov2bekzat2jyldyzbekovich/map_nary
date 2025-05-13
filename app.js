4bfcbb33772d4c52346ea94b5df8b78d6ca883fd
bekzat <bekzatajtbekzanov@gmail.com>
2025-05-13 19:34:40 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const { createApp, ref, onMounted } = Vue;
+
+createApp({
+  setup() {
+    // model
+    const select = [
+      { value: "", name: "Все" },
+      { value: "power", name: "Электростанции" },
+      { value: "minerals", name: "Минералы" },
+      { value: "future_power", name: "Строящиеся Электростанции" },
+      { value: "return_energy", name: "ВИЭ" },
+      { value: "other", name: "Прочее" },
+    ];
+
+    // init
+    const initMap = () => {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 41.433678, lng: 75.983283  },
+        zoom: 10,
+      });
+    };
+
+    window.initMap = initMap;
+    onMounted(() => {
+      initMap();
+    });
+
+    return {
+      select,
+    };
+  },
+}).mount("#app");
