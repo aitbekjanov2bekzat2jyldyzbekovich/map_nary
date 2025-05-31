44ee88c8471af7427f3726757a423642a8719a9a
bekzat <bekzatajtbekzanov@gmail.com>
2025-05-31 20:48:52 +0300

    add rout

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 import content from "./layout/content.js";
+
+const Home = {
+  template: `<layout-content></layout-content>`,
+};
+const Admin = {
+  template: `<layout-content></layout-content>`,
+};
+const routes = [
+  { path: "/", component: Home },
+  { path: "/admin", component: Admin },
+];
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+});
+
 const app = Vue.createApp({});
-app.component("content", content);
+app.component("layout-content", content);
+
+app.use(router);
 app.mount("#app");
