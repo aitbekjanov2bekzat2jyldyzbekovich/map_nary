d0b5d4e9aaf453fdc06d9504a1595346689e7ed9
bekzat <bekzatajtbekzanov@gmail.com>
2025-06-01 20:33:32 +0300

    add info

--- layout/content.js
+++ layout/content.js
@@ -63,26 +63,54 @@ export default {
     };
   },
   methods: {
+    async getData(name) {
+      const response = await fetch(
+        `https://narynmap-35e43-default-rtdb.firebaseio.com/${name}.json`
+      );
+      const ress = await response.json();
+      return await ress;
+    },
     initt() {
+      const self = this;
       async function initMap() {
         const map = new google.maps.Map(document.getElementById("map"), {
           center: { lat: 41.433678, lng: 75.983283 },
           zoom: 8,
         });
-
-        const response = await fetch(
-          "https://narynmap-35e43-default-rtdb.firebaseio.com/map.json"
-        );
-        const ress = await response.json();
-        console.log(ress);
-
-        map.data.addGeoJson(ress);
+        const naryn = await self.getData("map");
+        map.data.addGeoJson(naryn);
         map.data.setStyle({
           fillColor: "blue",
           strokeColor: "red",
           strokeWeight: 2,
           fillOpacity: 0.2,
         });
+
+        renderMarker();
+
+        async function renderMarker() {
+          const markers = await self.getData("markers");
+
+          await markers.data.forEach((m) => {
+            const marker = new google.maps.Marker({
+              position: { lat: m.lat, lng: m.lng },
+              map: map,
+            });
+            const infoWindow = new google.maps.InfoWindow({
+              content: `
+<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+<p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+</a>
+`,
+            });
+
+            marker.addListener("click", () => {
+              infoWindow.open(map, marker);
+            });
+          });
+        }
       }
 
       if (window.google && window.google.maps) {
