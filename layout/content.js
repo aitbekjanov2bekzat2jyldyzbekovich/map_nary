12499be7bbbc3b567bddb7233360eeacf5eefc01
bekzat <bekzatajtbekzanov@gmail.com>
2025-06-09 14:39:49 +0300

    hgghgh

--- layout/content.js
+++ layout/content.js
@@ -92,18 +92,32 @@ export default {
           const markers = await self.getData("markers");
 
           await markers.data.forEach((m) => {
+            console.log(m);
+
             const marker = new google.maps.Marker({
               position: { lat: m.lat, lng: m.lng },
               map: map,
             });
             const infoWindow = new google.maps.InfoWindow({
               content: `
-<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-
-<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-<p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-</a>
-`,
+              <div class="p-2">
+                 <div class="mb-5">
+                  <h3 class="mb-2 text-center text-blue-700 font-bold text-xl">${m.title}</h3>
+                                 <ul class="mb-5">
+      <li>
+        <span class="text-base font-bold">Адрес:</span>
+        <span class="text-sm font-medium">Ул. Самансур Ата 45</span>
+      </li>
+        <li>
+        <span class="text-base font-bold">Тип:</span>
+        <span class="text-sm font-medium">Электростанция</span>
+      </li>
+    </ul>
+                  <p class="text-base">${m.description}</p>
+                  </div>
+      
+                  <img class="w-full  h-68 " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiUEATVBgNVr7ri-sJMGwdrzd5QwDE9rZOEA&s" alt="">
+              </div>`,
             });
 
             marker.addListener("click", () => {
